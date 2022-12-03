c5e26684f3a394dbce59d1c4f92280d28c47c2b4
Pablo Valdes <pablovaldes0925@gmail.com>
2022-12-03 18:38:02 -0500

    routing

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,65 @@
-import React from 'react'
+import { 
+  Box, 
+  Button, 
+  Center, 
+  FormControl, 
+  FormErrorMessage, 
+  FormLabel, 
+  Heading, 
+  Input,
+  Link,
+  Text
+} from '@chakra-ui/react';
+import { REGISTER } from "lib/routes";
+import { Link as RouterLink } from 'react-router-dom';
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+  <Center w="100%" h="100vh">
+    <Box mx="1" maxW="md" p="9" borderWidth="1px" borderRadius="lg">
+      <Heading mb="4" size="lg" textAlign="center">
+      Log In
+      </Heading>
+
+      <form onSubmit={() => {}}>
+        <FormControl isInvalid={true} py="2">
+          <FormLabel>Email</FormLabel>
+          <Input type="email" placeholder="user@email.com"></Input>
+        <FormErrorMessage>This is an error message</FormErrorMessage>
+        </FormControl>
+        <FormControl isInvalid={true} py="2">
+          <FormLabel>Password</FormLabel>
+          <Input type="password" placeholder="password123"></Input>
+        <FormErrorMessage>This is an error message</FormErrorMessage>
+        </FormControl>
+        <Button 
+         mt="4"
+         type="submit" 
+         colorScheme="teal" 
+         size="md" 
+         w="full" 
+         isLoading={true} 
+         loadingText="Logging In"
+        >
+          Log In
+        </Button>
+      </form>
+ 
+      <Text fontSize="xlg" align="center" mt="6">
+        Don't have an account?{" "}
+        <Link 
+        as={RouterLink} //renders as ChakraUI link but has functionality of React router link
+        to={REGISTER} 
+        color="teal.800" 
+        fontWeight="medium" 
+        textDecor="underline"
+        _hover={{background: "teal.100" }}
+        >
+          Register
+        </Link>{" "}
+        instead!
+      </Text>
+    </Box>
+  </Center>
+  );
 }
